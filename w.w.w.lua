f36198cfb2bf52aa14c1570e46c22c4e8c87fe02
kismile36 <1931947991@qq.com>
2026-03-27 21:28:53 +0800

    Update w.w.w.lua

--- w.w.w.lua
+++ w.w.w.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/2c73cbafaa7eb4e700a74f97455b5815c948685d1eefb067171c4a08dc3805bb/download"))()
+getgenv().SCRIPT_KEY = "FuckYou"
+loadstring(game:HttpGet("https://suckmydick.com"))()
